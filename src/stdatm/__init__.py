a34e2233668b46d8cec577e8f9bd3e01b009bb12
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2024-02-12 15:08:24 +0100

    Removed usage of deprecated pkg_resources.

--- src/stdatm/__init__.py
+++ src/stdatm/__init__.py
@@ -1,5 +1,5 @@
 #  This file is part of StdAtm
-#  Copyright (C) 2021 ONERA & ISAE-SUPAERO
+#  Copyright (C) 2024 ONERA & ISAE-SUPAERO
 #  StdAtm is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -11,16 +11,21 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from pkg_resources import DistributionNotFound, get_distribution
+import sys
 
 from .atmosphere import Atmosphere, AtmosphereSI  # noqa: F401
 from .atmosphere_partials import AtmosphereWithPartials  # noqa: F401
 
+if sys.version_info >= (3, 10):
+    import importlib.metadata as importlib_metadata
+else:
+    import importlib_metadata
+
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = "StdAtm"
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = importlib_metadata.distribution(dist_name).version
+except importlib_metadata.PackageNotFoundError:
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
+    del importlib_metadata, sys
