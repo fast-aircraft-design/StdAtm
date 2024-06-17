89fb4433b4f7e6b07cca16668c62004e7607549f
Christophe DAVID <christophe.david@onera.fr>
2024-06-17 23:07:39 +0200

    Now using Ruff

--- src/stdatm/state_parameters.py
+++ src/stdatm/state_parameters.py
@@ -12,7 +12,6 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 from functools import lru_cache, singledispatch
 from numbers import Real
 from typing import Union
